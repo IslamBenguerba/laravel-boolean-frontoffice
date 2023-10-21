16d4616eddbe7cd5e89397caa357547e5a46ed5d
Islam.Benguerba <islam.benguerba@gmail.com>
2023-10-21 15:19:24 +0200

    Do button

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import Home from "./pages/Home.vue"
 import Contacts from "./pages/Contacts.vue"
 import AboutUs from "./pages/AboutUs.vue"
+import ButtonVue from "./components/Button.vue"
 
 export default{
   components:{
     Home,
     Contacts,
     AboutUs,
+    ButtonVue
   },
   data(){
 
@@ -18,9 +20,13 @@ export default{
 }
 </script>
 
+
+
 <template>
 <router-view></router-view>
 <h1>CAZZO</h1>
+<ButtonVue classe=" bg-danger" text="oooo"></ButtonVue>
+
 </template>
 
 <style lang="scss">
---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<script>
+
+export default{
+    props:
+    {
+        text :'',
+        classe:''
+    },
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+    }
+}
+</script>
+<template>
+
+<button class="btn" :class="classe"> Ciaone {{ text }}</button>
+</template>
+
+<style scoped>
+
+
+</style>
