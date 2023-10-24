bcc7f95468f9cedde25616e4884eded822ca960b
PietroFiori91 <pederflowers@hotmail.it>
2023-10-24 17:48:40 +0200

    add axios call and sliderino

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script>
-import Home from "./pages/Home.vue"
-import Contacts from "./pages/Contacts.vue"
-import AboutUs from "./pages/AboutUs.vue"
+import Home from "./pages/Home.vue";
+import Contacts from "./pages/Contacts.vue";
+import AboutUs from "./pages/AboutUs.vue";
 
-import TheHeader from "./components/TheHeader.vue"
-import TheMain from "./components/TheMain.vue"
+import TheHeader from "./components/TheHeader.vue";
+import TheMain from "./components/TheMain.vue";
 
-import Card from "./components/Card.vue"
-import LightCard from "./components/LightCard.vue"
-import MediumCard from "./components/MediumCard.vue"
-import Jumbotron from "./components/Jumbotron.vue"
+import Card from "./components/Card.vue";
+import LightCard from "./components/LightCard.vue";
+import MediumCard from "./components/MediumCard.vue";
+import Jumbotron from "./components/Jumbotron.vue";
+import Slider from "./components/Slider.vue";
 
 export default {
   components: {
@@ -22,17 +23,12 @@ export default {
     LightCard,
     MediumCard,
     Jumbotron,
+    Slider,
   },
-  data(){
-
-  },
-  methods: {
-
-  }
-}
+  data() {},
+  methods: {},
+};
 </script>
-
-
 
 <template>
   <div class="main-container">
@@ -45,12 +41,13 @@ export default {
     <TheNavbar></TheNavbar>
     <Jumbotron></Jumbotron>
 
-    <div class="placeholder-padding"> <!-- placeholder-->
+    <div class="placeholder-padding">
+      <!-- placeholder-->
       <Card></Card>
       <LightCard></LightCard>
       <MediumCard></MediumCard>
     </div>
-
+    <Slider></Slider>
   </div>
 </template>
 
@@ -60,14 +57,16 @@ export default {
 
 .main-container {
   background-color: $color-section-light;
-/*   padding: 0 3rem;
- */}
+  /*   padding: 0 3rem;
+ */
+}
 
-  .placeholder-padding {                  //placeholder
-    padding: 5rem 0;
-    display: flex;
-    gap: 5rem;
-  }
+.placeholder-padding {
+  //placeholder
+  padding: 5rem 0;
+  display: flex;
+  gap: 5rem;
+}
 </style>
 
 <!--
---
+++ src/components/Slider.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="container py-5">
+    <div class="slider-container">
+      <div
+        v-for="(slide, index) in slides"
+        :key="index"
+        class="slide"
+        :style="{
+          backgroundImage: `url(${slide.strDrinkThumb})`,
+          transform: `translateX(-${currentIndex * 100}%)`,
+        }"
+      >
+        <div class="slide-content p-3 text-center">
+          <h2 class="py-2 px-5">{{ slide.strDrink }}</h2>
+          <!-- <p>{{ slide.strInstructions }}</p> -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      slides: [],
+      currentIndex: 0,
+    };
+  },
+  methods: {
+    created() {
+      axios
+        .get("http://127.0.0.1:8000/api/cocktails")
+        .then((response) => {
+          this.slides = response.data;
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    },
+    nextSlide() {
+      this.currentIndex = (this.currentIndex + 1) % this.slides.length;
+    },
+  },
+  mounted() {
+    this.created();
+    setInterval(this.nextSlide, 5000);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.slider-container {
+  height: 500px;
+  width: 100%;
+  display: flex;
+  overflow: hidden;
+  position: relative;
+}
+
+.slider {
+  display: flex;
+  transition: transform 0.5s ease-in-out;
+}
+
+.slide {
+  flex: 0 0 100%;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+h2 {
+  font-size: 3rem;
+  color: #0f1d20;
+  background-color: #cfc3b3;
+}
+</style>
