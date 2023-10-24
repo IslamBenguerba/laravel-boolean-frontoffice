2b514c1e64db775df3985735f86a64565e76c86e
Islam.Benguerba <islam.benguerba@gmail.com>
2023-10-24 16:04:22 +0200

    Do Subtitle

---
+++ src/components/Subtitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    props:{
+
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+
+</script>
+<template>
+    <div class="edition-benghi"></div>
+</template>
+<style>
+
+</style>
