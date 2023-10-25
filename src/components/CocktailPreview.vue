783ed1a15094fbfa5a339badb0b1fdee6b92f06f
Islam.Benguerba <islam.benguerba@gmail.com>
2023-10-25 15:31:50 +0200

    Hope-V2

--- src/components/CocktailPreview.vue
+++ src/components/CocktailPreview.vue
@@ -29,73 +29,114 @@ export default {
 </script>
 
 <template>
-  <div class="cocktail-preview-main-container"> <!-- :class="{'hidden': isActive}" @toggle-hidden="toggleHidden" -->
-    <div class="cocktail-img-container">
-      <!-- <img :src="store.strDrinkThumb" alt="" v-if="drink !== null"> -->
-      <img :src="store.drinkView.strDrinkThumb" alt="" v-if="store.drinkView !== null">
-    </div>
+  <div class="fluid-container">
+    <div class="cocktail-preview-main-container classy-style" v-if="store.drinkView !== null">
+      <div class="cocktail-img-container classy-style">
+        <img class="fit-img" :src="store.drinkView.strDrinkThumb" alt="">
+      </div>
 
-    <div class="cocktail-data-container" v-if="store.drinkView !== null">
-      <!-- aggiungere la classe "cocktail-info" e fixare i bordi ai div giusti -->
-      <div>
-        {{ store.drinkView['strDrink'] }}
-        <div>
-          <h3>Gli ingredienti</h3>
-          <ul>
-            <li v-for="ingredient in store.drinkView['ingredienti']">{{ ingredient }}</li>
-          </ul>
-          <h3>I dosaggi</h3>
-          <ul>
-            <li v-for="dose in store.drinkView['misure']">{{ dose }}</li>
-          </ul>
-          <h3>Istruzioni</h3>
-          {{ store.drinkView['strInstructionsIT'] }}
+      <div class="cocktail-data-container classy-style">
+        <div class="row-container">
+          <div class="classy-style name-label">{{ store.drinkView['strDrink'] }}</div> <!-- NOME -->
+        </div>
+
+        <div class="row-container"> <!-- CATEGORIE -->
+          <div class="classy-style info-label">{{store.drinkView['strAlcoholic']}} </div>
+          <div class="classy-style info-label">{{ store.drinkView['strGlass'] }}</div>
+          <div class="classy-style info-label">{{ store.drinkView['strIBA'] }}</div>
+        </div>
+
+        <div class="row-container"> <!-- INGREDIENTI -->
+          <div class="classy-style info-label" v-for="ingredient in store.drinkView['ingredienti']">{{ ingredient }}</div>
+          <!--ciclo for per richiamare gli ingredienti non null-->
+        </div>
+        <div class="row-container">
+          <div class="classy-style info-label" v-for="dose in store.drinkView['misure']">{{ dose }}</div>
+          <!--ciclo for per richiamare le measures non null-->
+        </div>
+
+        <div class="row-container instruction-container"> <!-- ISTRUZIONI -->
+          <div class="just-bordered instruction-label">{{ store.drinkView['strInstructionsIT'] }}</div>
         </div>
       </div>
     </div>
   </div>
 
-  <div class="space-divisor"></div>
-
-  </template>
+  <div class="big-space-divisor"></div>
+</template>
 
 <style scoped lang="scss">
 @use "../../scss/partials/mixins" as *;
 @use "../../scss/partials/variables" as *;
 
-.space-divisor {
-  padding-bottom: 1rem;
-}
+  .space-divisor {
+    padding-bottom: 1rem;
+  }
 
-.cocktail-preview-main-container {
-  border: 1px solid $color-text-light; //temporaneo per lo sviluppo
-  height: 350px;
-  padding: 2rem;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  color: $color-text-light;
-}
+  .classy-style {                                       //GENERAL STYLE
+    border: 1px solid $color-text-light;
+    text-transform: capitalize;
+    font-weight: 600;
+  }
 
-// .hidden {
-//   display: none;
-// }
+  .just-bordered {
+    border: 1px solid $color-text-light;
+    font-weight: 500;
+  }
 
-.cocktail-img-container {
-  width: 20%;
+  .fluid-container {                                    //MAIN CONTAINERS
+    @include flex-center-center;
+  }
+
+  .row-container {
+    display: flex;
+    gap: 7px;
+  }
+
+  .cocktail-preview-main-container {
+    width: 1500px;
+    height: 350px;
+    padding: 2rem;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    color: $color-text-light;
+    @include flex-between-center;
+  }
+
+  .cocktail-img-container {
+    width: 20%;
+    height: 100%;
+  }
+
+  .cocktail-data-container {
+    width: 78%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+    padding: 1rem;
+  }
+
+  .instruction-container {
+    width: 100%;
+  }
+
+  .name-label,
+  .info-label {                                     //SINGLE LABEL
+    width: 150px;
+    text-align: center;
+  }
+
+  .instruction-label {
+    height: 100px;
+    width: 100%;
+    padding: 2px 10px;
+  }
+
+.fit-img{
   height: 100%;
-}
-
-.cocktail-data-container {
-  width: 75%;
-  height: 100%;
-  @include flex-col-center-center;
-}
-
-.cocktail-info {
-  //CONTAINER INFO
-  border: 1px solid $color-text-light;
-  text-transform: capitalize;
-  font-weight: 600;
+  width: 100%;
 }
 </style>
